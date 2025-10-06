1c6b4c6ffc267f9ae44cc78cb40cf28a0131afb9
ManoaLahatra <manoalahatra07@gmail.com>
2025-10-06 09:09:14 +0300

    chore: add cors

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import questionsRouter from './routes/questions.js';
 import answerRouter from './routes/answer.js';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/', questionsRouter);
 app.use('/', answerRouter);
